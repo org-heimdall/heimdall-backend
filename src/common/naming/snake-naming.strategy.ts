b10b417cbb573b631c1c8fe54af415b0e1cc292c
Godot-B <bae0821@gmail.com>
2026-07-09 12:52:24 +0900

    feat: 커스텀 snake case 전역 적용 (#12)

---
+++ src/common/naming/snake-naming.strategy.ts
@@ -0,0 +1,77 @@
+import {
+  DefaultNamingStrategy,
+  type NamingStrategyInterface,
+} from 'typeorm';
+import { snakeCase } from 'typeorm/util/StringUtils';
+
+export class SnakeNamingStrategy
+  extends DefaultNamingStrategy
+  implements NamingStrategyInterface
+{
+  tableName(targetName: string, userSpecifiedName: string | undefined): string {
+    return userSpecifiedName ?? snakeCase(targetName);
+  }
+
+  columnName(
+    propertyName: string,
+    customName: string | undefined,
+    embeddedPrefixes: string[],
+  ): string {
+    return snakeCase(
+      embeddedPrefixes
+        .concat(customName ?? propertyName)
+        .join('_'),
+    );
+  }
+
+  relationName(propertyName: string): string {
+    return snakeCase(propertyName);
+  }
+
+  joinColumnName(relationName: string, referencedColumnName: string): string {
+    return snakeCase(`${relationName}_${referencedColumnName}`);
+  }
+
+  joinTableName(
+    firstTableName: string,
+    secondTableName: string,
+    firstPropertyName: string,
+    _secondPropertyName: string,
+  ): string {
+    return snakeCase(
+      `${firstTableName}_${firstPropertyName.replaceAll(/\./gi, '_')}_${secondTableName}`,
+    );
+  }
+
+  joinTableColumnName(
+    tableName: string,
+    propertyName: string,
+    columnName?: string,
+  ): string {
+    return snakeCase(`${tableName}_${columnName ?? propertyName}`);
+  }
+
+  joinTableInverseColumnName(
+    tableName: string,
+    propertyName: string,
+    columnName?: string,
+  ): string {
+    return this.joinTableColumnName(tableName, propertyName, columnName);
+  }
+
+  override foreignKeyName(
+    ...[
+      tableOrName,
+      columnNames,
+      referencedTablePath,
+      referencedColumnNames,
+    ]: Parameters<DefaultNamingStrategy['foreignKeyName']>
+  ): ReturnType<DefaultNamingStrategy['foreignKeyName']> {
+    return super.foreignKeyName(
+      tableOrName,
+      columnNames.map((columnName) => snakeCase(columnName)),
+      referencedTablePath,
+      referencedColumnNames?.map((columnName) => snakeCase(columnName)),
+    );
+  }
+}
